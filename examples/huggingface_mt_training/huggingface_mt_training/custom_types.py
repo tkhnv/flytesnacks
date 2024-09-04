814cb957a68f8ebc10ea69d3a549ea5cf380c478
Ales Tamchyna <ales.tamchyna@memsource.com>
2024-09-04 16:08:46 +0200

    minor cleanup

--- examples/huggingface_mt_training/huggingface_mt_training/custom_types.py
+++ examples/huggingface_mt_training/huggingface_mt_training/custom_types.py
@@ -1,7 +1,5 @@
-import dataclasses
 from dataclasses import dataclass
 
-from datasets import Dataset
 from flytekit.types.structured.structured_dataset import StructuredDataset
 from mashumaro.mixins.json import DataClassJSONMixin
 
