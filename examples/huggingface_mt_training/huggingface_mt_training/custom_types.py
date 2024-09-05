c40e1d7e1f0bb3524d9ecae71606c3d6e81f4ff6
Maxim Tychonov <mtkhnv@proton.me>
2024-09-05 17:08:39 +0200

    add type support for metric names

--- examples/huggingface_mt_training/huggingface_mt_training/custom_types.py
+++ examples/huggingface_mt_training/huggingface_mt_training/custom_types.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Literal
 
 from flytekit.types.structured.structured_dataset import StructuredDataset
 from mashumaro.mixins.json import DataClassJSONMixin
@@ -9,3 +10,5 @@ class DatasetWithMetadata(DataClassJSONMixin):
     dataset: StructuredDataset
     source_language: str
     target_language: str
+
+Metric = Literal["bleu", "chrf"]
