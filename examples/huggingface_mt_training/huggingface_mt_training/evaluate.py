05f5e4f2d6d10eea2afb7cf3c2fd0f3d907a389e
Ales Tamchyna <ales.tamchyna@memsource.com>
2024-09-06 13:35:14 +0200

    bugfix in chrf

--- examples/huggingface_mt_training/huggingface_mt_training/evaluate.py
+++ examples/huggingface_mt_training/huggingface_mt_training/evaluate.py
@@ -6,7 +6,7 @@ from .custom_types import DatasetWithMetadata, Metric, EvaluateReturnType
 
 metric_name_to_score_map: dict[Metric, str] = {
     Metric.bleu: "bleu",
-    Metric.chrf: "score",
+    Metric.chrf: "chrf",
 }
 
 
@@ -18,11 +18,12 @@ def evaluate(
 ) -> EvaluateReturnType:
     from datasets import load_metric, Dataset
     import pandas as pd
+
     metric = load_metric(metric_name_to_score_map[metric_name], **load_metric_kwargs)
     structured_dataset = dataset.dataset
     hf_dataset = Dataset.from_pandas(structured_dataset.open(pd.DataFrame).all())
     # TODO: this doesn't crash, but the score is 0. Find out what exactly we need to pass as input to the metric
-    score = metric.compute(predictions=[[h] for h in hf_dataset["detokenized"]], references=[[[r]] for r in hf_dataset["target"]])[
-        metric_name_to_score_map[metric_name]
-    ]
+    score = metric.compute(
+        predictions=[[h] for h in hf_dataset["detokenized"]], references=[[[r]] for r in hf_dataset["target"]]
+    )[metric_name_to_score_map[metric_name]]
     return EvaluateReturnType(score=score)
