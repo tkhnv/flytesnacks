1f6b50e92ebed1dcdbaaecad4683e064cb32098e
Ales Tamchyna <ales.tamchyna@memsource.com>
2024-09-06 11:15:18 +0200

    add plotly

--- examples/huggingface_mt_training/huggingface_mt_training/image_specs.py
+++ examples/huggingface_mt_training/huggingface_mt_training/image_specs.py
@@ -2,6 +2,16 @@ from flytekit import ImageSpec
 
 # AutoModelForSeq2SeqLM requires torch to be installed
 transformers_image_spec = ImageSpec(
-    packages=["transformers", "torch", "pandas", "datasets", "flytekitplugins-huggingface", "sentencepiece", "pandas", "accelerate"],
+    packages=[
+        "transformers",
+        "torch",
+        "pandas",
+        "datasets",
+        "flytekitplugins-huggingface",
+        "sentencepiece",
+        "pandas",
+        "accelerate",
+        "plotly",
+    ],
     registry="localhost:30000",
 )
